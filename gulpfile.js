f0a2d2bb6344fc30e1e6b361c0c84fcc6f3ed5ab
juanjopareja <jupg25@msn.com>
2023-05-03 18:25:26 +0200

    Add .webp and .avif images

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
 
@@ -7,15 +10,60 @@ function css( done ) {
         .pipe( plumber() )
         .pipe( sass() )
         .pipe( dest("build/css") )
-    
     done();
 }
 
+
+// IMAGES
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
+function webpVersion( done ) {
+    const options = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( webp(options) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+function avifVersion( done ) {
+    const options = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( avif(options) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+function images( done ) {
+    const options = {
+        optimizationLevel: 3
+    }
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( cache( imagemin(options) ) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+
+// DEV
 function dev( done ) {
-    watch("src/scss/**/*.scss", css)
-
+    watch("src/scss/**/*.scss", css);
     done();
 }
 
+
+// EXPORTS
 exports.css = css;
-exports.dev = dev;
+exports.images = images;
+exports.webpVersion = webpVersion;
+exports.avifVersion = webpVersion;
+exports.dev = parallel( images, webpVersion, avifVersion, dev );
